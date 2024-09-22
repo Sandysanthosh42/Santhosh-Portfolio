2263dd656dc880dc40829c9021d500167967bd16
Santhosh Babu L R <152953636+Sandysanthosh42@users.noreply.github.com>
2024-09-22 11:50:33 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var typed=new Typed(".text",{
+    strings :["Software Developer","FrontEnd Developer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+})
+
+document.getElementById('contact-form') .addEventListener('submit',function (event){
+    event.preventDefault();
+    var data = new FormData(event.target);
+    fetch("https://formsubmit.co/el/zanopo",{
+        method:"POST",
+        body:data,
+    }).then(response =>{
+        if (response.ok) {
+            alert('Success');
+        } else{
+            alert('Error');
+        }
+    });
+    document.getElementById('contact-form').reset();
+});
